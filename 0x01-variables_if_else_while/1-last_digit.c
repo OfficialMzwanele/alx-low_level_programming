d1ec5a8acbffa3f1baa11642c3e2a6d00672df3e
OfficialMzwanele <mzwamft@gmail.com>
2023-03-17 12:12:22 -0700

    1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -6,27 +6,25 @@
  * Return: Always 0
  * This code assesses the last digit of the entered number.
  */
-
 int main(void)
 {
-	int n, last_digit;
+	int n, last_d;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	scanf("%d", &n);
-	/*Finding the remainder of the given number by dividing 10*/
-	last_digit = n % 10;
-	if (last_digit > 5)
+	last_d = n % 10;
+	if (last_d > 5)
 	{
-		printf("Last digit of %d is %d is and greater than 5\n", n, last_digit);
+		printf("Last digit of %d is %d and is greater than 5\n", n, last_d);
 	}
-	else if (last_digit == 0)
+	else if (last_d == 0)
 	{
-		printf("Last digit of %d is %d and is 0\n", n, last_digit);
+		printf("Last digit of %d is %d and is 0\n", n, last_d);
 	}
-	else if ((last_digit < 6) && (last_digit != 0))
+	else if ((last_d < 6) && (last_d != 0))
 	{
-		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, last_digit);
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, last_d);
 	}
 	return (0);
 }

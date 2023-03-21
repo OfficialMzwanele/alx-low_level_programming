6eeac4dd6b0bc054e8df97d4a3460d47bbb66146
OfficialMzwanele <mzwamft@gmail.com>
2023-03-21 11:39:37 -0700

    4-isalpha.c

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -7,8 +7,14 @@
  */
 int _isalpha(int c)
 {
-	if (c >= 'a' && c <= 'z' && c >= 'A' && c <= 'Z')
+	if (c >= 'a' && c <= 'z')
+	{
 		return (0);
+	}
+	else if (c >= 'A' && c <= 'Z')
+	{
+		return (0);
+	}
 	else
 		return (1);
 }

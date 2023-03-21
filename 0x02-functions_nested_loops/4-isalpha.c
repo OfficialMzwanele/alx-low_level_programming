8f433f859a8549f5c421c70a6a630037644df691
OfficialMzwanele <mzwamft@gmail.com>
2023-03-21 11:14:16 -0700

    4-isalpha.c

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _isalpha - a function that checks for lower and upper cases
+ * @c: single letter input
+ * Return: 1 if c is a letter, lowercase or uppercase, 0 if otherwise
+ */
+int _isalpha(int c)
+{
+	if (c >= 'a' && c <= 'z' && c >= 'A' && c <= 'Z')
+		return (0);
+	else
+		return (1);
+}
